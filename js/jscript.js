dd5d9c0a66b0ad04613d96670234d7b2f9e56767
sqwalbran <67843456+sqwalbran@users.noreply.github.com>
2021-04-11 02:11:39 -0500

    Update jscript.js

--- js/jscript.js
+++ js/jscript.js
@@ -83,8 +83,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  false); // Explicitly setting the flag to get JSON from server processed into an object literal
+  buildAndShowHomeHTML(categories), // ***** <---- TODO: STEP 1: Substitute [...] ******
+  true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
