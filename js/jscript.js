ccd75202a8e740c3f0ed7950b8ef4101764123c4
sqwalbran <67843456+sqwalbran@users.noreply.github.com>
2021-04-13 09:31:15 -0500

    Update jscript.js

--- js/jscript.js
+++ js/jscript.js
@@ -82,11 +82,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,buildAndShowHomeHTML)
- // allCategoriesUrl, function(buildAndShowHomeHTML) {
- //   document.querySelector("#main-content")
- //     .innerHTML = buildAndShowHomeHTML.value;
- // },// ***** <---- TODO: STEP 1: Substitute [...] ******
+  //allCategoriesUrl,buildAndShowHomeHTML)
+ allCategoriesUrl, function(buildAndShowHomeHTML) {
+   document.querySelector("#main-content")
+     .innerHTML = buildAndShowHomeHTML.value;
+ },// ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server 
 });
 // *** finish **
